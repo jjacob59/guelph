1f2b526a7ea8824860565699859bd0202c442c84
hallett <michael.t.hallett@mcgill.ca>
2021-09-22 09:37:09 -0400

    fixed terrifying mistake.

--- src/grace/configs/gray/exp22.py
+++
@@ -1,8 +0,0 @@
-
-_base_=[ 'config1.py' ]
-
-total_epochs = 1000
-
-optimizer = dict(lr=0.01, momentum=0.9, weight_decay=0.001, paramwise_cfg=dict(bias_lr_mult=2., bias_decay_mult=0.))
-
-load_from = '/home/data/refined/deep-microscopy/output/final_experiment/exp22/latest.pth'
--- src/grace/configs/pseudohyphae/exp44.py
+++
@@ -1,5 +0,0 @@
-_base_ = [ 'config.py' ]
-
-optimizer = dict(lr=0.01, momentum=0.9, weight_decay=0.0001, paramwise_cfg=dict(bias_lr_mult=2., bias_decay_mult=0.))
-
-load_from = '/home/data/refined/deep-microscopy/output/mike_curriculum/exp44/latest.pth'
